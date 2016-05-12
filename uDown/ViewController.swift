241ea77d42cd53e01a9d3d60e07b9389931884c9
Oscar Pan <oscarpan888@gmail.com>
2016-05-12 14:33:13 -0700

    Firebase and Facebook login integrated

--- uDown/ViewController.swift
+++ uDown/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let ref = Firebase(url: "https://udown.firebaseio.com")
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"], handler: {
+            (facebookResult, facebookError) -> Void in
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                print("Facebook login was cancelled.")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                ref.authWithOAuthProvider("facebook", token: accessToken,
+                    withCompletionBlock: { error, authData in
+                        if error != nil {
+                            print("Login failed. \(error)")
+                        } else {
+                            print("Logged in! \(authData)")
+                        }
+                })
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
