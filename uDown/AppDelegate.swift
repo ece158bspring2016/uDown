c2db1ec016be1058ac6255a012bf0bee6ffdf35d
Rachit Sengupta <srachit@gmail.com>
2016-05-13 21:12:37 -0700

    Fixed Facebook login and firebase user data

--- uDown/AppDelegate.swift
+++ uDown/AppDelegate.swift
@@ -15,14 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //Checks if user is fb authenticated if not 
+    //it asks user to login
     func application(application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return FBSDKApplicationDelegate.sharedInstance()
             .application(application, didFinishLaunchingWithOptions: launchOptions)
     }
+    
+    //Activated facebook
     func applicationDidBecomeActive(application: UIApplication) {
         FBSDKAppEvents.activateApp()
     }
+    
+    //Opens and handles the facebook url authentication page
     func application(application: UIApplication, openURL url: NSURL,
                      sourceApplication: String?, annotation: AnyObject?) -> Bool {
         return FBSDKApplicationDelegate.sharedInstance()
--- uDown/ViewController.swift
+++ uDown/ViewController.swift
@@ -18,15 +18,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
+      }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func facebookLogin(sender: AnyObject)
+    {
         let ref = Firebase(url: "https://udown.firebaseio.com")
+        
         let facebookLogin = FBSDKLoginManager()
+        print("Logging In")
         facebookLogin.logInWithReadPermissions(["email"], handler: {
             (facebookResult, facebookError) -> Void in
             if facebookError != nil {
                 print("Facebook login failed. Error \(facebookError)")
+                
             } else if facebookResult.isCancelled {
                 print("Facebook login was cancelled.")
             } else {
+                
                 let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
                 ref.authWithOAuthProvider("facebook", token: accessToken,
                     withCompletionBlock: { error, authData in
@@ -34,15 +47,25 @@ class ViewController: UIViewController {
                             print("Login failed. \(error)")
                         } else {
                             print("Logged in! \(authData)")
+                            
+                            let newUser = [
+                                "provider": authData.provider,
+                                "displayName": authData.providerData["displayName"] as? NSString as? String,
+                                "email": authData.providerData["email"] as? NSString as? String
+                            ]
+                            
+                            ref.childByAppendingPath("users")
+                            .childByAppendingPath(authData.uid).setValue(newUser)
+                            
+                            //Display next view controller
+                            
+                          /*  let nextView = (self.storyboard?.instantiateInitialViewControllerWithIdentifier("NumberPad"))! as UIViewController self.presentViewController(nextView, animated: true, completion: nil)*/
                         }
                 })
             }
         })
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
