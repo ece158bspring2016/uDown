134a9c5f4ece8f27464cf86256ac7d0e22f9b856
Rachit Sengupta <srachit@gmail.com>
2016-05-29 21:39:12 -0700

    Selected activity is returned

--- uDown/SearchCreateViewController.swift
+++ uDown/SearchCreateViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class SearchCreateViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var activityButton: UIButton!
+    var activity:String = "Grab Food" {
+        didSet {
+            activityButton.setTitle(activity, forState: UIControlState.Normal)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +29,15 @@ class SearchCreateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cancelToSearchCreateViewController(segue:UIStoryboardSegue) {
+    }
+    
+    @IBAction func doneSearchCreateViewController(segue:UIStoryboardSegue) {
+        if let activityPickerViewController = segue.sourceViewController as? ActivityPickerViewController,
+            selectedActivity = activityPickerViewController.selectedActivity {
+                activity = selectedActivity
+        }
+    }
     
 
     /*
